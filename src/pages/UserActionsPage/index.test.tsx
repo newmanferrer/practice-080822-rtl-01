f8ee11272340f239ebad60061f770d12d8fcd975
Newman Ferrer <newmanferrer@gmail.com>
2022-08-13 14:42:45 -0400

    UserActionsPage add new test

--- src/pages/UserActionsPage/index.test.tsx
+++ src/pages/UserActionsPage/index.test.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { render, screen, fireEvent, findByText } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { UserActionsPage } from './index';
 
 describe('test <FiringEventsPage />', () => {
@@ -43,11 +43,50 @@ describe('test <FiringEventsPage />', () => {
   //* ----------------------------------------------------------------------------------------------
   //* Async Methods Examples
   //* ----------------------------------------------------------------------------------------------
-  it('test #3 (): should ', async () => {
+  it('test #3 (findByText/await): should the "Button -1", click just one time', async () => {
     const button = screen.getByRole('button', { name: '-1' });
 
     fireEvent.click(button);
     expect(await screen.findByText('Counter: -1')).toBeInTheDocument();
+  });
+
+  it('test #4 (getByText/waitFor): should the "Button +1", click just two times and "Button Reset", click just one time', async () => {
+    const buttonAddOne = screen.getByRole('button', { name: '+1' });
+    const buttonReset = screen.getByRole('button', { name: 'Reset' });
+
+    fireEvent.click(buttonAddOne);
+    fireEvent.click(buttonAddOne);
+    fireEvent.click(buttonReset);
+
+    await waitFor(() => expect(screen.getByText('Counter: 0')).toBeInTheDocument());
+  });
+
+  it('test #5 (findByText/waitFor): should the "Button -1", click just two times and "Button Reset", click just one time', async () => {
+    const buttonSubtractOne = screen.getByRole('button', { name: '-1' });
+    const buttonReset = screen.getByRole('button', { name: 'Reset' });
+
+    fireEvent.click(buttonSubtractOne);
+    fireEvent.click(buttonSubtractOne);
+    fireEvent.click(buttonReset);
+
+    await waitFor(async () => expect(await screen.findByText('Counter: 0')).toBeInTheDocument());
+  });
+  //* ----------------------------------------------------------------------------------------------
+
+  //* ----------------------------------------------------------------------------------------------
+  //* Considerations for fireEvent
+  //* ----------------------------------------------------------------------------------------------
+  it('test #6: should the "button +1", click just two times', () => {
+    const buttonAdd = screen.getByRole('button', { name: '+1' });
+
+    fireEvent.mouseMove(buttonAdd);
+    fireEvent.mouseOver(buttonAdd);
+    fireEvent.mouseDown(buttonAdd);
+    fireEvent.mouseUp(buttonAdd);
+    fireEvent.click(buttonAdd);
+    fireEvent.click(buttonAdd);
+
+    expect(screen.getByText('Counter: 2')).toBeInTheDocument();
   });
   //* ----------------------------------------------------------------------------------------------
 });
